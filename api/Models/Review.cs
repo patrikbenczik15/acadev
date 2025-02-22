216e5c733c908803a79c2285f2cb2b70d3675ca8
Patrik Benczik <benczikp.roberto@gmail.com>
2025-02-22 22:24:33 +0200

    Updated project architecture
    added dao, repositories, services packages

--- api/Models/Review.cs
+++ api/Models/Review.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
-namespace api.Models
+namespace api.models
 {
     public class Review
     {
--- api/dao/ProfessorDao.cs
+++ api/dao/ProfessorDao.cs
@@ -5,25 +5,22 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
 
-namespace api.models
+namespace api.dao
 {
     public class Professor
     {
         public int Id { get; set; }
 
-        [Required]
         public string FirstName { get; set; } = string.Empty;
 
-        [Required]
         public string LastName { get; set; } = string.Empty;
 
-        [Required]
         public string University { get; set; } = string.Empty;
 
         public string Department { get; set; } = string.Empty;
 
 
         // ! review list for professor
-        public ICollection<Review> Reviews { get; set; } = new List<Review>();
+        // public ICollection<Review> Reviews { get; set; } = new List<Review>();
     }
 }
