0ce4879a660ec369e205dfd90985e0fe2e49be50
Patrik Benczik <benczikp.roberto@gmail.com>
2025-02-23 04:34:34 +0200

    CONNECTED DB TO BACKEND
    lets goooo

--- api/dao/ApplicationDbContext.cs
+++ api/dao/ApplicationDbContext.cs
@@ -5,11 +5,10 @@ namespace api.Dao
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
-
+        
         public DbSet<Professor> Professors { get; set; }
         public DbSet<Review> Reviews { get; set; }
         public DbSet<User> Users { get; set; }
