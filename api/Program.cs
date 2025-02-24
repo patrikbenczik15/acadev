7f6ff3e3b918e24e81eb2b08d9626d2627daa76d
Patrik Benczik <benczikp.roberto@gmail.com>
2025-02-24 21:46:38 +0200

    Update Program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Injecting ApplicationDbContext 
+// ! Injecting ApplicationDbContext 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
